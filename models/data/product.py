cfa8d161cbd2ed79d9b7f07308dffb543c979ac9
rluizaga <rluizaga@fi.uba.ar>
2023-06-11 20:52:28 -0300

    Se modifican valores iniciales de la tabla product

--- models/data/product.py
+++ models/data/product.py
@@ -14,8 +14,8 @@ class Product(Base):
 def initialize_db():
     db = SessionLocal()
 
-    item1 = Product(name="Item 1", version="Description 1")
-    item2 = Product(name="Item 2", version="Description 2")
+    item1 = Product(name="Siu Guarani", version="versio 1.0")
+    item2 = Product(name="Siu Guarani", version="version 1.1")
 
     db.add(item1)
     db.add(item2)
