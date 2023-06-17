3e8dba375f340d83cbc1328c99683c06c2be8edb
rluizaga <rluizaga@fi.uba.ar>
2023-06-17 16:56:38 -0300

    Se agrega en docker-compose

--- app/database/connection.py
+++ app/database/connection.py
@@ -2,11 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
+from app.config import settings
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+engine = create_engine(settings.db_url)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
