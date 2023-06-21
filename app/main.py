47a77c81ae89668f024adc4cebde64720faf7dbc
rluizaga <rluizaga@fi.uba.ar>
2023-06-21 18:40:30 -0300

    Se agrega cors

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.controllers import product_controllers, client_controllers, ticket_controllers
 from app.database.connection import Base, engine
@@ -9,3 +10,11 @@ app = FastAPI()
 app.include_router(product_controllers.router)
 app.include_router(client_controllers.router)
 app.include_router(ticket_controllers.router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
