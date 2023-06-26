6ff6033471e99ff856ebf7afd1a645458d5c5773
rluizaga <rluizaga@fi.uba.ar>
2023-06-26 18:38:29 -0300

    Se agregan mas productos

--- app/controllers/product_controllers.py
+++ app/controllers/product_controllers.py
@@ -4,8 +4,11 @@ from app.models.request.product import Product
 router = APIRouter()
 
 products = [
-    Product(id=1, name="Siu Guarani", version="1.0"),
-    Product(id=2, name="Siu Guarani", version="2.0"),
+    Product(id=1, name="Siu Guarani", version="1.0.0"),
+    Product(id=2, name="Siu Guarani", version="2.0.0"),
+    Product(id=3, name="Gestion de Inventario", version="1.0.0"),
+    Product(id=4, name="Sistema de Tickets", version="1.0.0"),
+    Product(id=5, name="Sistema de Molinetes", version="1.0.0")
 ]
 
 
